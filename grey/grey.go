75872ffa37c2712305f28c2cce3daa24914ca39b
hydezhao <hydezhao2@gmail.com>
2022-11-22 12:23:37 +0800

    feat[zzz]: add grey plugin

---
+++ grey/grey.go
@@ -0,0 +1,30 @@
+package main
+
+// This file is here to test features of the Go compiler.
+
+import (
+	"context"
+	"github.com/anthonynsimon/bild/effect"
+	"github.com/anthonynsimon/bild/imgio"
+	plugin "imgPlugin/proto"
+)
+
+type greyPlugin struct {
+}
+
+func (g *greyPlugin) Modify(context context.Context, request plugin.PluginRequest) (plugin.PluginReply, error) {
+	image, err := imgio.Open(request.SrcPath)
+	if err != nil {
+		return plugin.PluginReply{Message: err.Error()}, err
+	}
+	image = effect.Grayscale(image)
+	err = imgio.Save(request.DestPath, image, imgio.JPEGEncoder(int(request.Quality)))
+	if err != nil {
+		return plugin.PluginReply{Message: "failed "}, err
+	}
+	return plugin.PluginReply{Message: "succ"}, nil
+}
+
+func main() {
+	plugin.RegisterActuator(&greyPlugin{})
+}
